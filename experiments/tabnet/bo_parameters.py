4280ce0fe219040445390f10264f7f69de011f50
Alexander <knopochnik@mail.ru>
2021-04-01 14:08:57 +0000

    bo hyperparams optimized, kdd dataset added

--- experiments/tabnet/bo_parameters.py
+++ experiments/tabnet/bo_parameters.py
@@ -15,9 +15,12 @@ SEARCH_SPACE_XGB = [
 SEARCH_SPACE_TABNET = [
     # Real(0.01, 1.0, name='lr'),
     Real(1.0, 2.0, name='gamma'),
-    Real(0.001, 0.1, name='lambda_sparse'),
+    Real(0.0001, 0.1, name='lambda_sparse'),
     Integer(3, 10, name='n_steps'),
-    Categorical([8, 16, 24, 32, 40, 48, 54, 64], name='n_a'),
+    Categorical([8, 16, 24, 32, 64], name='n_a'),
+    Categorical([0.6, 0.7, 0.8, 0.9, 0.95, 0.98], name='momentum'),
+    Categorical([4096, 8192], name='batch_size'),
+    Categorical([128, 256], name='virtual_batch_size'),
 ]
 
 LIGHTGBM_PARAMS = {
@@ -39,5 +42,5 @@ XGBOOST_PARAMS = {
 
 TABNET_PARAMS = {
         'seed': 42,
-        'verbose': False,
+        'verbose': True,
 }
